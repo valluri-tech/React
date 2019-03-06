24eb45e9db236f0fee70453a8016c09e6f6d8e5a
satya-valluri <prakash4455@gmail.com>
2019-03-06 12:26:43 +0530

    Dynamic Loading Explained using Import and React.Lazy

---
+++ ReactWebSite/code-splitting/README.md
@@ -0,0 +1,4 @@
+1.
+This Project 'code-splitting' will explain about code splitting technique in React.This helps in reduction of the main bundle size and loading a part of the JS dynamically if the user needs else it would never be loaded.
+
+2. With React.lazy you can render a Dynamic import as a regular component.
---
+++ ReactWebSite/code-splitting/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+//import {add} from './math';
+const OtherComponent = React.lazy(() => import('./OtherComponent'));
+
+class App extends React.Component {
+
+    getAddition = (a, b) => {
+        import("./math").then(
+            (OBJ) => {
+                console.log(OBJ.add(a, b));
+            }
+        )
+    }
+
+    render() {
+        return (
+            <div>
+                {/*{  this.getAddition(5,5)   }*/}
+                <React.Suspense fallback={<div>Loading Other Component</div>} >
+                    <OtherComponent />
+                </React.Suspense>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
