10b134ed493519bd9d6e9c44ae1bed8880db8f54
satya-valluri <prakash4455@gmail.com>
2019-03-08 16:55:03 +0530

    Adding a Ref to a DOM Element

--- ReactWebSite/refs/src/index.js
+++ ReactWebSite/refs/src/index.js
@@ -2,26 +2,41 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import AppErrorBoundary from './ErrorBoundary/AppErrorBoundary'
 
-class App extends React.Component {
+class CustomTextInput extends React.Component {
 
     constructor(props) {
         super(props);
         this.textInput = React.createRef()
     }
 
-    focusTextInput = ()=>{
+    focusTextInput = () => {
         this.textInput.current.focus();
     }
 
     render = () => {
         return (
             <div>
-                <input type= 'text' ref={this.textInput}/>
-                <input type= 'button' onClick={this.focusTextInput} value="Click to Focus on Input"/>
+                <input type='text' ref={this.textInput} />
+                <input type='button' onClick={this.focusTextInput} value="Click to Focus on Input" />
             </div>
         );
     }
 }
 
+class AutoFocusTextInput extends React.Component {
+    constructor(props) {
+        super(props);
+        this.textInput = React.createRef();
+    }
+    componentDidMount = () => {
+        this.textInput.current.focusTextInput();
+    }
+    render = () => {
+        return (
+            <CustomTextInput ref={this.textInput} />
+        )
+    }
+}
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<CustomTextInput />, document.getElementById('root'));
+ReactDOM.render(<AutoFocusTextInput />, document.getElementById('root'));
