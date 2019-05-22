ab9a8f44706c7fafcfbf6246500dac1b80167077
Walluri <prakash4455@gmail.com>
2019-05-22 18:08:09 +0530

    Redux Counter : Store Implemented. [not reflected in UI]

--- Redux/reduxcounters/src/CounterStore.js
+++ Redux/reduxcounters/src/CounterStore.js
@@ -1,12 +1,19 @@
 // import { createStore } from 'redux'
 const { createStore } = require('redux')
 
-const counterReducer = (state, action) => {
+const counterReducer = (state = { counterBlob: new Map("", "") }, action) => {
     switch (action.type) {
         case 'ADD_COUNTER':
-            break;
+            return AddCounter(state, action.payload);
+        // break;
+        case 'INCREMENT_COUNTER':
+            return IncrementCounter(state, action.payload)
+        // break;
+        case 'DECREMENT_COUNTER':
+            return DecrementCounter(state, action.payload)
         case 'REMOVE_COUNTER':
             break;
+
         default:
             return state;
     }
@@ -16,28 +23,44 @@ const Store = createStore(counterReducer);
 
 //lets maintain an array of counters
 
-const AddCounter = (list) => {
-    //list.push(0);
-    //return list;
-
-    //return a new list
-    //return list.concat([0]);
-
-    //when ever we want to add a counter we just want a placeholder of its value as 0.
-    return [...list,0];
+const AddCounter = (state, { key, value }) => {
+    let newMap = new Map(state.counterBlob);
+    newMap.set(key, value);
+    let newState = { counterBlob: newMap }
+    return newState;
 }
 
-const RemoveCounter = (list,index)=>{
+const IncrementCounter = ({ counterBlob }, { key }) => {
+    let newMap = new Map(counterBlob);
+    let val = newMap.get(key) + 1;
+    console.log('+' + val)
+    newMap.set(key, val++);
+    let obj = { counterBlob: newMap };
+    return obj;
+}
+
+const DecrementCounter = (state, { key }) => {
+    let newMap = new Map(state.counterBlob);
+    let val = newMap.get(key)-1;
+    console.log('-' +  val)
+    newMap.set(key,val);
+    let obj = {
+        counterBlob:newMap
+    }
+    return obj;
+}
+
+const RemoveCounter = (list, index) => {
 
     //delete list[index]; - leaves a whole
-    
+
     //list.splice(0,index+1);
     //return list;
     //first index'th element is excluded  on the next slice the index'th element is included
     //return list.slice(0,index).concat(list.slice(index+1)); same as below
-    return [...list.slice(0,index),...list.slice(index+1)]
+    return [...list.slice(0, index), ...list.slice(index + 1)]
 }
 
-module.exports = { Store, AddCounter, RemoveCounter};
+module.exports = { Store, AddCounter, RemoveCounter };
 
 
--- Redux/reduxcounters/src/index.js
+++ Redux/reduxcounters/src/index.js
@@ -1,58 +1,98 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { Store } from './CounterStore'
+
 const uudiV4 = require('uuid/v4')
 
 const Counter = (props) => {
+    console.log('rendering : Counter ');
     return (
         <div>
-            <br /> {0} <br />
-            <button>+</button><button>-</button>
+            <br />{(props.id).substring(0, 9).concat("...")}<br />
+            <br />{props.val(props.id)}<br />
+            <button id={props.id} onClick={props.inc}>+</button>
+            <button id={props.id} onClick={props.dec}>-</button>
             <br />
         </div>
     )
 }
 
 const App = () => {
+
+    console.log("Rendering App");
+
     const [Elements, SetElements] = useState({
         inputCollection: new Map()
     });
 
     const [currentID, setcurrentID] = useState("");
-    
-    const makeNewCounter = () => <Counter />
+
+    const makeNewCounter = (key) => <Counter id={key} inc={IncrementCounter} dec={DecrementCounter} val={getValue} />
+
+    const getValue = (id) => {
+        console.log('getvalue called...!');
+        const st = Store.getState();
+        //console.log(st.counterBlob.get(id));-
+        return st.counterBlob.get(id);
+    }
 
     const AddCounter = () => {
         const newMap = new Map(Elements.inputCollection);
         const key = uudiV4();
-        newMap.set(key,makeNewCounter())
+        newMap.set(key, makeNewCounter(key))
         SetElements({
-            inputCollection:newMap
+            inputCollection: newMap
+        })
+        Store.dispatch({
+            type: 'ADD_COUNTER',
+            payload: {
+                key: key,
+                value: 10
+            }
         })
     }
 
-    const DeleteCounter = ()=>{
+    const IncrementCounter = (e) => {
+        Store.dispatch(
+            {
+                type: 'INCREMENT_COUNTER',
+                payload: {
+                    key: e.target.id
+                }
+            }
+        );
+    }
+
+    const DecrementCounter = (e) => {
+        Store.dispatch(
+            {
+                type: 'DECREMENT_COUNTER',
+                payload: {
+                    key: e.target.id
+                }
+            }
+        );
+    }
+
+    const DeleteCounter = (e) => {
         const newMap = new Map(Elements.inputCollection);
         newMap.delete(currentID);
         SetElements({
-            inputCollection:newMap
+            inputCollection: newMap
         })
         setcurrentID("")
-    }
- 
-    const listItemSelected = (e)=> {
-        setcurrentID((e.target.id).toString());
     }
 
     const ulStyle = {
         'listStyle': 'none'
     }
- 
+
     const liStyle = {
-        width : '150px',
-        backgroundColor:'pink',
-        paddingLeft : '20px',
-        paddingBottom : '20px',
-        marginTop:'10px'
+        width: '150px',
+        backgroundColor: 'pink',
+        paddingLeft: '20px',
+        paddingBottom: '20px',
+        marginTop: '10px'
     }
 
     const arrElements = [...Elements.inputCollection]
@@ -63,7 +103,14 @@ const App = () => {
 
             <ul style={ulStyle}>
                 {
-                    arrElements.map( ([key,value])=>  <li style={liStyle} key={key} id={key} onClick={listItemSelected}>{value}</li> )
+                    arrElements.map(([key, value]) =>
+                        <li style={liStyle}
+                            key={key}
+                            id={key}
+                            onClick={(e) => {
+                                setcurrentID((e.target.id).toString())
+                            }} >{value}</li>
+                    )
                 }
             </ul>
             {currentID}
@@ -71,7 +118,12 @@ const App = () => {
         </>
     );
 }
-ReactDOM.render(<App />, document.getElementById('root'));
+
+Store.subscribe(() => {
+    //console.log("rendering App")
+    ReactDOM.render(<App />, document.getElementById('root'))
+});
+Store.dispatch({ type: null });
 
 
- 
+
