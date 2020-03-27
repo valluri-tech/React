917dc8dec731845cdebdb08cf22566f84b8b1524
walluri <prakash4455@gmail.com>
2020-03-27 18:02:49 +0530

    React Hooks Cheat sheet : useEffect hook

--- ReactWebSite/hooks/statehook/src/index.js
+++ ReactWebSite/hooks/statehook/src/index.js
@@ -1,24 +1,14 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-function App() {
-    const [count, setCount] = useState(0);
-    let btnStyle = {
-        width:'100px',
-        height:'25px'
-    }
+const App = () => {
     return (
         <div>
-            <p>You have clicked {count} times</p>
-            <button onClick={ () => {setCount(count+1)} } style={btnStyle}>Click</button>
+            H            
         </div>
-    )
+   );
+            H}
+
+RactDOM.render(<App />, document.getElementById('root'));
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-//1. useState is a hook
-//2. we call it inside a function component to add some local state to the component.
-//3. useState returns two values - the current state value and a function that lets you update it.
-//4. what is a hook : Hooks are functions that let you 'hookinto' React state and life cycle features - FROM, FUNCTION COMPONENTS.
-//5. we can also create our own hooks to re-use stateful behaviour between different components.
